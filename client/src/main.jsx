1f4c757e9170116a902a0e440355388d3070abb8
Frisk17 <syed2302995@st.jmi.ac.in>
2024-02-23 19:21:58 +0530

    Generated a client react project with vite

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Root from './Root.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+	<Root></Root>
+	</React.StrictMode>,
+)
