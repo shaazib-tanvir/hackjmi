c563cc80f0e14f937acd565d7d2d2dac0070e9ac
Frisk17 <syed2302995@st.jmi.ac.in>
2024-02-23 20:53:03 +0530

    Added dark theme

--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -1,7 +1,17 @@
+import {ThemeProvider} from "@emotion/react";
+import {CssBaseline, createTheme} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function Root() {
 	return (
-		<>
-		</>
+		<ThemeProvider theme={darkTheme}>
+		<CssBaseline></CssBaseline>
+		</ThemeProvider>
 	);
 }
 
