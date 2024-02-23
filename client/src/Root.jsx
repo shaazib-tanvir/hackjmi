f62182f4c5d5431294c55a9bf2866af2f351ecdc
Frisk17 <syed2302995@st.jmi.ac.in>
2024-02-24 02:04:46 +0530

    Implemented react-dom-router

--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -1,9 +1,21 @@
 import {ThemeProvider} from "@emotion/react";
 import {CssBaseline, createTheme} from "@mui/material";
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import NotFound from "./views/NotFound";
+
+const router = createBrowserRouter(createRoutesFromElements(
+	<>
+		<Route errorElement={<NotFound></NotFound>} path="/" element={<Login></Login>}></Route>
+		<Route errorElement={<NotFound></NotFound>} path="/login" element={<Login></Login>}></Route>
+		<Route errorElement={<NotFound></NotFound>} path="/register" element={<Register></Register>}></Route>
+	</>
+));
 
 const darkTheme = createTheme({
 	palette: {
-		mode: 'dark',
+		mode: "dark",
 	},
 });
 
@@ -11,6 +23,7 @@ function Root() {
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<CssBaseline></CssBaseline>
+			<RouterProvider router={router}></RouterProvider>
 		</ThemeProvider>
 	);
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Root from './Root.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Root from './Root';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
